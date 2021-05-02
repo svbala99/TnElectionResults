8afca10e90574ab1e5dbcec5b4f0c23aae4f4361
Balamurugan V <svbala99@gmail.com>
2021-05-02 11:33:49 +0530

    URL

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {WebView} from 'react-native-webview';
 class App extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-        <Text>Hello world</Text>
-      </View>
+      <WebView
+        source={{
+          uri:
+            'https://results.eci.gov.in/Result2021/partywiseresult-S22.htm?st=S22/',
+        }}
+      />
     );
   }
 }
